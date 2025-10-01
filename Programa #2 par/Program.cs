7093b060943cd62080f2c4313c288900c7663b35
yohana0939 <yohanamontero447@gmail.com>
2025-09-30 22:57:30 -0400

    Add files via upload

---
+++ Programa #2 par/Program.cs
@@ -0,0 +1,26 @@
+﻿//Student name: Yohana Montero
+//Student ID: 2025-0939
+//class day : viernes
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Enter a number 
+        Console.Write("Digite un numero:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("The number:" + number + " es par");
+        }
+        else
+        {
+            Console.WriteLine("The number:" + number + " es impar");
+        }
+    }
+}
+
